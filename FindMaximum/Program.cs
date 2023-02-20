89c8104b079f3ed2576d947354a3b5e57b3981cb
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-02-20 08:28:24 +0530

    [Ritesh]add : Find Maximum Using Three Integer

---
+++ FindMaximum/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            integerfindmax integerfindmax1 = new integerfindmax();
+            integerfindmax1.FindMaxValue(10, 30, 40);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FindMaximum/integerfindmax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMaximum
+{
+    public class integerfindmax
+    {
+ 
+        public void FindMaxValue(int first, int second, int third)       //UC1 finding max of 3 numbers.
+        {
+            if (first.CompareTo(second) >= 0 && first.CompareTo(third) >= 0)
+            {
+                Console.WriteLine($"{first} is greater");
+            }
+            else if (second.CompareTo(first) >= 0 && second.CompareTo(third) >= 0)
+            {
+                Console.WriteLine($"{second} is greater");
+            }
+            else if (third.CompareTo(first) >= 0 && third.CompareTo(second) >= 0)
+            {
+                Console.WriteLine($"{third} is greater ");
+            }
+            else
+                Console.WriteLine("All Numbers are Equal! ");
+        }
+    }
+}
