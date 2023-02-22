6ac9ea47251af12288192046c0633511967bf036
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-02-22 08:38:07 +0530

    [Ritesh]add : Find Maximum Test Case Of The Maximum Genrics Of Float Number

--- FindMaxIntTest/UnitTest1.cs
+++ FindMaxIntTest/UnitTest1.cs
@@ -11,20 +11,20 @@ namespace FindMaxIntTest
         [Test]
         public void GivenMaxFirstNum_WhenAnalysed_ShouldreturnFirstMax()
         {
-            int result = genericsMethodForInt.FindMaxValue(20, 10, 17);
-            Assert.AreEqual(20, result);
+            double result = genericsMethodForFloat.FindMaxValue(20.40f, 10.50f, 17.67f);
+            Assert.AreEqual(20.40f, result);
         }
         [Test]
         public void GivenMaxSecondNum_WhenAnalysed_ShouldreturnSecondMax()
         {
-            int result = genericsMethodForInt.FindMaxValue(10, 20, 17);
-            Assert.AreEqual(20, result);
+            double result = genericsMethodForFloat.FindMaxValue(10.45f, 20.78f, 17.65f);
+            Assert.AreEqual(20.78f, result);
         }
         [Test]
         public void GivenMaxThirdNum_WhenAnalysed_ShouldreturnThirdMax()
         {
-            int result = genericsMethodForInt.FindMaxValue(10, 17, 20);
-            Assert.AreEqual(20, result);
+            double result = genericsMethodForFloat.FindMaxValue(10.65f, 17.34f, 20.25);
+            Assert.AreEqual(20.25f, result);
         }
 
     }
